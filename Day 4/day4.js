8a2d1fe8b4a1fdacb8056bce1148d977bcb74469
Sufyan Badshah <sufyanbadshah898@gmail.com>
2024-03-17 18:33:11 +0500

    Add files via upload

---
+++ Day 4/day4.js
@@ -0,0 +1,16 @@
+// // 16/03/24
+// // this prints a message for variable Name
+// let Name = "Sufyan"
+// console.log(`Hey ${Name} do you wanna learn some typescript today?`)
+// // 16/03/24
+// // this prints a message too
+// console.log (`Khabib Once Said,"I don't train to fight I train to Dominate"`);
+// let names : string [] = ["Ali", "Huzaifa","Hasnain","Ayan"]
+// for (let i = 0; i < names.length; i++) {
+//     console.log(names[i]);
+// }
+var names = ["Ali", "Huzaifa", "Hasnain", "Ayan"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hey ".concat(name_1, ", what scene?"));
+}
---
+++ Day 4/day4.ts
@@ -0,0 +1,17 @@
+// 16/03/24
+// this prints a message for variable Name
+let Name = "Sufyan"
+console.log(`Hey ${Name} do you wanna learn some typescript today?`)
+// 16/03/24
+// this prints a message too
+console.log (`Khabib Once Said,"I don't train to fight I train to Dominate"`);
+
+let names : string [] = ["Ali", "Huzaifa","Hasnain","Ayan"]
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+    
+}
+
+let names : string [] = ["Ali", "Huzaifa","Hasnain","Ayan"]
+for(let name of names)
+console.log (`Hey ${name}, what scene?`)
